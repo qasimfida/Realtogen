3db5305226a2ecf2081edbc82d84c8a8b4516ec8
qasimfida <qasimfida0054@gmail.com>
2025-03-14 02:07:39 +0500

    changes

--- src/app/Components/Footer/Footer.jsx
+++ src/app/Components/Footer/Footer.jsx
@@ -9,17 +9,17 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col pb-[18px] bg-[#141A30] md:pl-[220px] md:pr-[196px] px-[14px]">
-      <div className="flex flex-col-reverse md:flex-row max-sm:items-center md:gap-[208px] pt-[38px] pb-[19px] ">
-        <div className="max-sm:pt-[64px]">
-          <div className="max-sm:pb-[32px] max-sm:flex max-sm:flex-col max-sm:gap-[32px]">
+    <div className="flex flex-col pb-[18px] bg-[#141A30] lg:pl-[220px] lg:pr-[196px] px-[14px]">
+      <div className="flex flex-col-reverse sm:flex-col sm:items-center lg:flex-row max-sm:items-center lg:gap-[208px] sm:gap-[28px]  pt-[38px] pb-[19px] ">
+        <div className="max-sm:pt-[64px] w-full">
+          <div className="max-sm:pb-[32px] max-sm:flex max-sm:flex-col max-sm:gap-[32px] ">
             <p className="text-[#ffffff83]">Social Media</p>
             <h1 className="font-semibold text-[32px] text-white">
               Join our community!
             </h1>
           </div>
           <div className="flex gap-[16px] md:pt-[63px]">
-            <div className="bg-[#f241ae4d] w-[100px] md:w-[123px] h-[100px] md:h-[123px] rounded-2xl flex flex-col items-center justify-center">
+            <div className="bg-[#F241AE26] w-[100px] md:w-[123px] h-[100px] md:h-[123px] rounded-2xl flex flex-col items-center justify-center">
               <InstaIcon />{" "}
               <Link
                 href={"#"}
@@ -28,7 +28,7 @@ const Footer = () => {
                 Insta
               </Link>
             </div>
-            <div className="bg-[#426bff5d] w-[100px] md:w-[123px]  h-[100px] md:h-[123px] rounded-2xl flex flex-col items-center justify-center">
+            <div className="bg-[#426BFF26]  w-[100px] md:w-[123px]  h-[100px] md:h-[123px] rounded-2xl flex flex-col items-center justify-center">
               <FacebookIcon />
               <Link
                 href={"#"}
@@ -37,7 +37,7 @@ const Footer = () => {
                 Facebook
               </Link>
             </div>
-            <div className="bg-[#d83e3e71] w-[100px] md:w-[123px]  h-[100px] md:h-[123px] rounded-2xl flex flex-col items-center justify-center">
+            <div className="bg-[#D83E3E26] w-[100px] md:w-[123px]  h-[100px] md:h-[123px] rounded-2xl flex flex-col items-center justify-center">
               <YoutubeIcon />
               <Link
                 href={"#"}
@@ -47,21 +47,32 @@ const Footer = () => {
               </Link>
             </div>
           </div>
-          <div className="pt-[29px]">
-            <h1 className="text-[#ffffff83]">Quick Links</h1>
-            <div className="flex max-sm:flex-col gap-[25px] text-[16px] font-semibold text-white">
-              <Link href={"/"}>Home</Link>
-              <Link href={"/"}>Gallery</Link>
-              <Link href={"/"}>Features</Link>
-              <Link href={"/"}>Pricing</Link>
-              <Link href={"/"}>Blogs</Link>
+          <div className="max-sm:flex pt-[29px] justify-between">
+            <div className="">
+              <h1 className="text-[#ffffff83] max-sm:mb-[23px]">Quick Links</h1>
+              <div className="flex max-sm:flex-col gap-[32px] text-[16px] font-semibold text-white">
+                <Link href={"/"}>Home</Link>
+                <Link href={"/"}>Gallery</Link>
+                <Link href={"/"}>Features</Link>
+                <Link href={"/"}>Pricing</Link>
+                <Link href={"/"}>Blogs</Link>
+              </div>
+
+            </div>
+
+            <div className="hidden max-sm:flex flex-col gap-[25px] text-[16px] font-semibold text-white">
+              <h1 className="text-[#ffffff83]"> Terms & Conditions</h1>
+              <Link href={"/"}>Terms of Service</Link>
+              <Link href={"/"}>Refund Policy</Link>
+              <Link href={"/"}>Privacy Policy</Link>
+              <Link href={"/"}>Cookie Policy</Link>
             </div>
           </div>
         </div>
-        <div >
-          <div className="max-sm:flex max-sm:flex-col max-sm:gap-[32px]">
+        <div className="mt-[14px]">
+          <div className="max-sm:flex max-sm:flex-col max-sm:gap-[32px] max-sm:px-[13px]">
             <p className="text-[#ffffff83]">Newsletter</p>
-            <h1 className="font-semibold text-[24px] md:text-[32px] text-white">
+            <h1 className="font-semibold text-[24px] md:text-[32px] text-white py-[11px] max-sm:py-[32px]">
               Stay connected
             </h1>
             <p className="text-[16px] font-normal text-white">
@@ -69,16 +80,16 @@ const Footer = () => {
               directly in your inbox.
             </p>
           </div>
-          <div className="mt-[32px] max-sm:gap-[13px] md:mt-[47px] flex justify-center items-center rounded-2xl bg-white h-[63px] w-auto md:w-[413px]">
+          <div className="mt-[32px] max-sm:gap-[13px] md:mt-[47px] flex justify-between px-[7px] items-center rounded-2xl  bg-white h-[63px] w-[95%] mx-auto md:w-[413px] max-sm:px-[13px]">
             <Input
               placeholder="Enter your email address"
-              className={"w-auto md:w-[78%] border-white bg-white"}
+              className={"w-auto md:w-[78%] shadow-none  bg-white focus-visible:ring-0 focus:ring-0 focus:border-transparent border-none outline-none"}
             ></Input>
-            <Button variant={"primary"}>
-              <Link href={"#"}>Sign up</Link>
+            <Button variant="primary" className="px-4 py-6">
+              <Link href="#">Sign up</Link>
             </Button>
           </div>
-          <div className="pt-[42px] hidden">
+          <div className="pt-[42px] max-sm:hidden">
             <h1 className="text-[#ffffff83]">Terms & Conditions</h1>
             <div className="flex gap-[25px] text-[16px] font-semibold text-white">
               <Link href={"/"}>Terms of Service</Link>
