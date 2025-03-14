7a933deebdb54b978321361ec500388922cb971f
Qasim Fida <qasimfida0054@gmail.com>
2025-03-14 23:55:18 +0500

    changes

--- src/app/Components/comparison/Comparison.jsx
+++ src/app/Components/comparison/Comparison.jsx
@@ -7,9 +7,14 @@ import {
   AlertTriangle,
   DollarSign,
   Clock,
+  ShieldIcon,
 } from "lucide-react";
 import DollerIcon from "../../../../public/icons/DollerIcon";
 import SpeedIcon from "../../../../public/icons/SpeedIcon";
+import MoneybagIcon from "../../../../public/icons/MoneybagIcon";
+import ChartIcon from "../../../../public/icons/ChartIcon";
+import WarningIcon from "../../../../public/icons/WarningIcon";
+import SheildIcon from "../../../../public/icons/SheildIcon";
 
 const Comparison = () => {
   return (
@@ -28,42 +33,42 @@ const Comparison = () => {
         <div className="flex flex-col md:flex-row items-center justify-center gap-[16px]">
           {/* Realtogen.ai Section */}
           <div className="flex flex-col items-center">
-            <Card className="bg-[#2545B7] text-white rounded-2xl pl-[40px] pr-[30px] pt-[30px] pb-[45px] md:w-md shadow-xl">
+            <Card className="bg-[#2545B7] text-white rounded-2xl pl-[40px] pr-[30px] pt-[30px] pb-[45px] md:w-md ">
               <CardContent className="px-0">
                 <h3 className="text-[32px] pl-[7px] text-start font-semibold mb-4">
                   Realtogen.ai
                 </h3>
                 <ul className="space-y-3 text-sm text-white text-left">
-                  <li className="flex items-start gap-2">
-                    <ShieldCheck className="text-white mt-1 " />
+                  <li className="flex items-center gap-2">
+                    <SheildIcon className="text-white mt-1 " />
                     <p className=" text-[16px] font-normal">
                       <span className="font-bold">Instant Results:</span>{" "}
                       Generate aerial-style videos in minutes
                     </p>
                   </li>
-                  <li className="flex items-start gap-2">
-                    <ShieldCheck className="text-white mt-1" />
+                  <li className="flex items-center gap-2">
+                    <SheildIcon className="text-white mt-1" />
                     <p className=" text-[16px] font-normal">
                       <span className="font-bold">No On‐Site Filming:</span> AI
                       handles everything-no pilot, no crew
                     </p>
                   </li>
-                  <li className="flex items-start gap-2">
-                    <ShieldCheck className="text-white mt-1" />
+                  <li className="flex items-center gap-2">
+                    <SheildIcon className="text-white mt-1" />
                     <p className=" text-[16px] font-normal">
                       <span className="font-bold">Cost‐Effective: </span>Pay
                       only a fraction of what real drone shoots cost
                     </p>
                   </li>
-                  <li className="flex items-start gap-2">
-                    <ShieldCheck className="text-white mt-1" />
+                  <li className="flex items-center gap-2">
+                    <SheildIcon className="text-white mt-1" />
                     <p className=" text-[16px] font-normal">
                       <span className="font-bold"> Brand Customization: </span>
                       Easily add logos, colors, voiceovers, and subtitles
                     </p>
                   </li>
-                  <li className="flex items-start gap-2">
-                    <ShieldCheck className="text-white mt-1" />
+                  <li className="flex items-center gap-2">
+                    <SheildIcon className="text-white mt-1" />
                     <p className=" text-[16px] font-normal">
                       <span className="font-bold">Anytime, Anywhere: </span>
                       Create videos on demand from any device
@@ -77,18 +82,18 @@ const Comparison = () => {
               <div className="flex items-center gap-[38px]">
                 <div className="flex items-center gap-[38px]">
                   <DollerIcon />
-                  <h3 className="font-semibold text-2xl">16.90$</h3>
+                  <h3 className="font-semibold text-[24px]">16.90$</h3>
                 </div>
-                <div className="flex items-center gap-2">
+                <div className="flex items-center text-[16px] gap-2">
                   <p>pro plan price</p>
                 </div>
               </div>
               <div className="flex items-center gap-[44px]">
                 <div className="flex items-center gap-[48px]">
                   <SpeedIcon />
-                  <h3 className="font-semibold text-2xl">8 min</h3>
+                  <h3 className="font-semibold text-[24px]">8 min</h3>
                 </div>
-                <div className="flex items-center gap-2">
+                <div className="flex items-center text-[16px] gap-2">
                   <p>average delivery time</p>
                 </div>
               </div>
@@ -99,42 +104,42 @@ const Comparison = () => {
           <div className="text-[32px] font-semibold text-[#141A30]">VS</div>
 
           <div className="flex flex-col items-center">
-          <Card className="bg-[#FEF2F2] text-white rounded-2xl pl-[40px] pr-[30px] pt-[30px] pb-[45px] md:w-md shadow-xl">
+          <Card className="bg-[#FEF2F2] text-[#141A30] rounded-2xl pl-[40px] pr-[30px] pt-[30px] pb-[45px] md:w-md ">
               <CardContent className="px-0">
                 <h3 className="text-[32px] pl-[7px] text-start font-semibold mb-4">
                 Traditional Drone Filming
                 </h3>
-                <ul className="space-y-3 text-sm text-white text-left">
-                  <li className="flex items-start gap-2">
-                    <ShieldCheck className="text-white mt-1" />
+                <ul className="space-y-3 text-sm text-[#141A30] text-left">
+                  <li className="flex items-centercenter gap-2">
+                    <WarningIcon className="text-white mt-1" />
                     <p className=" text-[16px] font-normal">
                       <span className="font-bold">Instant Results:</span>{" "}
                       Generate aerial-style videos in minutes
                     </p>
                   </li>
-                  <li className="flex items-start gap-2">
-                    <ShieldCheck className="text-white mt-1" />
+                  <li className="flex items-center gap-2">
+                    <WarningIcon className="text-white mt-1" />
                     <p className=" text-[16px] font-normal">
                       <span className="font-bold">No On‐Site Filming:</span> AI
                       handles everything-no pilot, no crew
                     </p>
                   </li>
-                  <li className="flex items-start gap-2">
-                    <ShieldCheck className="text-white mt-1" />
+                  <li className="flex items-center gap-2">
+                    <WarningIcon className="text-white mt-1" />
                     <p className=" text-[16px] font-normal">
                       <span className="font-bold">Cost‐Effective: </span>Pay
                       only a fraction of what real drone shoots cost
                     </p>
                   </li>
-                  <li className="flex items-start gap-2">
-                    <ShieldCheck className="text-white mt-1" />
+                  <li className="flex items-center gap-2">
+                    <WarningIcon className="text-white mt-1" />
                     <p className=" text-[16px] font-normal">
                       <span className="font-bold"> Brand Customization: </span>
                       Easily add logos, colors, voiceovers, and subtitles
                     </p>
                   </li>
-                  <li className="flex items-start gap-2">
-                    <ShieldCheck className="text-white mt-1" />
+                  <li className="flex items-center gap-2">
+                    <WarningIcon className="text-white mt-1" />
                     <p className=" text-[16px] font-normal">
                       <span className="font-bold">Anytime, Anywhere: </span>
                       Create videos on demand from any device
@@ -145,20 +150,24 @@ const Comparison = () => {
             </Card>
 
             {/* Small box outside the card */}
-            <div className="mt-3 flex flex-col space-y-2 text-sm bg-[#FEF2F2]  rounded-xl text-[#141A30] w-full py-6 px-10">
-              <div className="flex items-center gap-2">
-                <DollarSign className="text-[#F05555]" />
-                <span>
-                  <strong className="font-semibold text-2xl">450$</strong>{" "}
-                  average cost in US
-                </span>
+            <div className="mt-3 flex flex-col space-y-2 text-sm bg-[#FEF2F2] p-4 rounded-xl text-[#141A30] w-full py-6 px-10">
+              <div className="flex items-center gap-[38px]">
+                <div className="flex items-center gap-[38px]">
+                  <MoneybagIcon />
+                  <h3 className="font-semibold text-[24px]">450$</h3>
+                </div>
+                <div className="flex items-center text-[16px] gap-2">
+                  <p>pro plan price</p>
+                </div>
               </div>
-              <div className="flex items-center gap-2">
-                <Clock className="text-[#F05555]" />
-                <span>
-                  <strong className="font-semibold text-2xl">3 days</strong>{" "}
-                  average delivery time
-                </span>
+              <div className="flex items-center gap-[30px]">
+                <div className="flex items-center gap-[48px]">
+                  <ChartIcon />
+                  <h3 className="font-semibold text-[24px]">3 days</h3>
+                </div>
+                <div className="flex items-center text-[16px] gap-2">
+                  <p>average delivery time</p>
+                </div>
               </div>
             </div>
           </div>
